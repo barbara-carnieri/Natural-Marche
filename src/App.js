d5f4f2856243a3d80800c1d95ee904609ed94404
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-03-17 14:00:18 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+import Cart from './components/Cart'
+import Details from './components/Details'
+import Default from './components/Default'
+import ProductList from './components/ProductList'
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        Hello from App!
+       <Navbar />
+       <ProductList />
+       <Details />
+       <Cart />
+       <Default />
       </React.Fragment>
     )
   }
