5fcf08cf24f30b96d23193a72c1123ca73bd69c8
barbara-carnieri <barbaracarnieri@yahoo.com>
2020-03-21 13:53:54 +0100

    paypal btn functional

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PaypalExpressBtn from 'react-paypal-express-checkout';
- 
+
 export default class MyApp extends React.Component {
     render() {
+        
         const onSuccess = (payment) => {
             // Congratulation, it came here means everything's fine!
                 console.log("The payment was succeeded!", payment);
                 this.props.clearCart();
-                this.props.history.push('/');
+                this.props.history.push('/home');
             		// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
         }
  
@@ -30,8 +31,7 @@ export default class MyApp extends React.Component {
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            // sandbox: process.env.REACT_APP_APP_ID,
-            sandbox: 'YOUR-SANDBOX-APP-ID',
+            sandbox: process.env.REACT_APP_APP_ID,
             production: 'YOUR-PRODUCTION-APP-ID',
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
